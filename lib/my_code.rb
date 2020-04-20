c35d59055c3134db8322d009f1ebb21323b04ae9
James Stamps Jr <jstampshere@gmail.com>
2020-04-20 15:58:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,9 +12,15 @@ end
 
 
   def reduce(source_array, starting_point = nil)
-    sum = starting_point
-        source_array.each do |x|
-        sum = yield(sum, x)
+    if starting_point  
+      sum = starting_point
+      i = 0
+    else
+      sum = source_array[0]
+      i = 1
+    end
+        while i < source_array.length
+        sum = yield(sum, source_array[i])
       end
     sum
   end
