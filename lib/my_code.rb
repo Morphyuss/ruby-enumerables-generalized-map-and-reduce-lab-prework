950efc9c6689cd5cedc1f153791d9755493e1c0e
James Stamps Jr <jstampshere@gmail.com>
2020-04-20 15:53:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,7 +11,7 @@ def map(source_array)
 end
 
 
-  def reduce(source_array, starting_point = 0)
+  def reduce(source_array, starting_point = nil)
     sum = starting_point
         source_array.each do |x|
         sum = yield(sum, x)
