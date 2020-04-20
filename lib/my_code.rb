716a6f9a304ba531aacc4c6e6c4cbdda6a0cc66d
James Stamps Jr <jstampshere@gmail.com>
2020-04-20 15:47:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,7 +10,7 @@ def map(source_array)
       new_array
 end
 
-
+require 'pry'
   def reduce(source_array, starting_point = nil)
       if starting_point
         sum = starting_point
@@ -20,79 +20,9 @@ end
         i = 1
       end
         while i < source_array.length
+          binding.pry
         sum += yield(sum, source_array[i])
         i += 1
         end
     sum
   end
-
-#  source_array.reduce(starting_point) { |sum, n| sum + n }
-=begin
-def map_to_negativize(source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( source_array[i] * -1 )
-    i += 1
-  end
-  return new
-end
-=end
-
-
-
-
-
-
-=begin
-def my_own_map(source_array)
-  source_array.map {|n| n*1}
-end
-
-def my_own_map(source_array)
-  source_array.map { |n| n*2 }
-end
-
-def my_own_map(source_array)
-  source_array.map { |n| (n*n).to_i }
-end
-=end
-
-
-
-=begin
-def map_to_negativize(source_array)
-  source_array.map { |i| i * -1 }
-end
-
-def map_to_no_change(source_array)
-  source_array.map
-  source_array
-end
-
-def map_to_double(source_array)
-  source_array.map { |i| i * 2 }
-end
-
-def map_to_square(source_array)
-  source_array.map { |i|  (i*i).to_i  }
-end
-
-def reduce_to_total(source_array, starting_point = 0)
-#  source_array.reduce(starting_point) { |sum, n| sum + n }
-  sum = starting_point
-      source_array.each do |x|
-      sum += x
-    end
-  sum
-end
-
-
-def reduce_to_all_true(source_array)
-    source_array.all?
-end
-
-def reduce_to_any_true(source_array)
-  source_array.any?
-end
-=end
